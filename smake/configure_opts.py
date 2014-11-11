ca5bb625759f1dd4a45182fd5196a0e083f0a4df
Juhász Viktor <viktor.juhasz@balabit.com>
2014-11-11 12:39:00 +0100

    smake: add this module to the smake framework
    Signed-off-by: Juhász Viktor <viktor.juhasz@balabit.com>

---
+++ smake/configure_opts.py
@@ -0,0 +1,1 @@
+default_opts="--disable-tcl --disable-readline --disable-amalgamation --enable-shared --disable-static  --disable-tcl --enable-threadsafe  --enable-cross-thread-connections --enable-debug"
