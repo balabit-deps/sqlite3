362f9486486a76c84d0e0da21b089039fa18b1aa
Juhász Viktor <viktor.juhasz@balabit.com>
2014-11-19 14:16:09 +0100

    build: Added platforms file and extended builder
    We need sqlite3.h-bb becuase the builder machines usually
    have not tclsh binary, but normally tclsh is required by creating sqlite3.h
    
    Signed-off-by: Juhász Viktor <viktor.juhasz@balabit.com>

--- sbuild/builder.py
+++ sbuild/builder.py
@@ -1,7 +1,23 @@
 from AutotoolsBuilder import AutotoolsBuilder
+import Utils
+
+class SqliteBuilder(AutotoolsBuilder):
+    def bootstrap(self):
+        result = super(SqliteBuilder, self).bootstrap() and super(SqliteBuilder, self).configure()
+        self.make("sqlite3.h")
+        Utils.run_command("mv '%s/sqlite3.h' '%s/sqlite3.h-bb'"%(self.build_dir, self.build_dir))
+        self.make("distclean")
+        return result
+
+    def configure(self, configure_opts = None):
+        result = super(SqliteBuilder, self).configure(configure_opts)
+        Utils.run_command("cp '%s/sqlite3.h-bb' '%s/sqlite3.h'"%(self.build_dir, self.build_dir))
+        Utils.run_command("touch '%s/sqlite3.h'"%(self.build_dir))
+        return result
+        
 
 def get_builder():
-    return AutotoolsBuilder(get_default_config_opts())
+    return SqliteBuilder(get_default_config_opts())
 
 def get_default_config_opts():
     try:
